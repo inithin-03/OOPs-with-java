58664ec08759c74beb2f7e287bf2be23dac0357e
SAI NITHIN DINTAKURTHI <2000050009@kluniversity.in>
2022-11-27 16:59:02 +0530

    Create Polymorphism Runtime.java

---
+++ Polymorphism Runtime.java
@@ -0,0 +1,38 @@
+package MethodOverriding;
+
+public class Student1 {
+	
+	public void studentinfo() {
+		System.out.println("I am Student1");
+	}
+	
+}
+public class Student2 extends Student1{
+	
+	public void studentinfo() {
+		System.out.println("I am Student2");
+	}
+	
+}
+public class Student3 extends Student1{
+
+	public void studentinfo() {
+		System.out.println("I am Student3");
+	}
+	
+}
+public class Main {
+	public static void main(String[] args)
+	{
+		Student1 st;
+		
+		st = new Student1();
+		st.studentinfo();
+		
+		st = new Student2();
+		st.studentinfo();
+		
+		st = new Student3();
+		st.studentinfo();
+	}
+}
