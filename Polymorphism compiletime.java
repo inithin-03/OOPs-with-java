a53bb9bf18ecd1d96d25d35cb19b9db9f6ec7be8
SAI NITHIN DINTAKURTHI <2000050009@kluniversity.in>
2022-11-27 07:59:30 +0530

    Update Polymorphism compiletime.java

--- Polymorphism compiletime.java
+++ Polymorphism compiletime.java
@@ -1,1 +1,40 @@
+package MethodOverloading;
 
+public class Student {
+	String name;
+	double cgpa;
+	
+	public void printInfo(String name)
+	{
+		System.out.println("Name of student is :"+ name);
+	}
+	
+	public void printInfo(double cgpa)
+	{
+		System.out.println("CGPA of student is :"+ cgpa);
+	}
+	
+	public void printInfo(String name,double cgpa)
+	{
+		System.out.println("Name of student is :"+ name + " CGPA of student is :"+ cgpa);
+	}
+	
+	public void printInfo(double cgpa,String name)
+	{
+		System.out.println("CGPA of student is :"+ cgpa+" Name of student is :"+name);
+	}
+}
+
+package MethodOverloading;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		Student stu = new Student();
+		
+		stu.printInfo("Virat");
+		stu.printInfo(9.5);
+		stu.printInfo("Virat", 9.5);
+		stu.printInfo(9.5,"Virat");
+	}
+}
